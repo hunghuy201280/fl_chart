b5bb04536835f5e38c0a8ef643b09688f82d4120
Truong Hung Huy <huyth12@cyberspace.vn>
2023-03-20 10:54:54 +0700

    update elevation

--- lib/src/chart/line_chart/line_chart_painter.dart
+++ lib/src/chart/line_chart/line_chart_painter.dart
@@ -1357,10 +1357,6 @@ class LineChartPainter extends AxisChartPainter<LineChartData> {
     }
   }
 
-  static double convertRadiusToSigma(double radius) {
-    return radius * 0.57735 + 0.5;
-  }
-
   @visibleForTesting
   double getBarLineXLength(
     LineChartBarData barData,
